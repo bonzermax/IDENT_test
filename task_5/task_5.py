71796a86fefc7f7ada5d70ed78ae346a5d55da8b
bonzermax <maksgames225@mail.ru>
2026-04-02 15:09:09 +0300

    feat: Решение задачи 5 (1 вариант)

---
+++ task_5/task_5.py
@@ -0,0 +1,31 @@
+import random
+import time
+from datetime import datetime, timedelta
+from concurrent.futures import ProcessPoolExecutor
+
+random.seed(42)
+
+receptions = [
+    {
+        "patient_id": pid,
+        "reception_start": datetime(2017, 6, 30)
+        - timedelta(days=random.randint(1, 500)),
+    }
+    for pid in range(1, 500_001)
+    for _ in range(random.randint(0, 5))
+]
+
+patients = [{"id": pid, "surname": f"Иванов{pid}"} for pid in range(1, 500_001)]
+
+# Решение №1
+time_start = time.perf_counter()
+receptions_le_2017 = {
+    r["patient_id"] for r in receptions if r["reception_start"] <= datetime(2017, 1, 1)
+}
+patients = [p for p in patients if p["id"] in receptions_le_2017]
+time_end = time.perf_counter()
+
+result_time = time_end - time_start
+print(f"Время выполнения решения №1: {result_time:.3f}")
+
+# Решение №2
