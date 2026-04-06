abf7c8e37d18ab73f7a48134381631e91dd7b80c
bonzermax <maksgames225@mail.ru>
2026-04-06 21:39:11 +0300

    update: Дополнено решение задания №5

--- task_5/task_5.py
+++ task_5/task_5.py
@@ -29,3 +29,18 @@ result_time = time_end - time_start
 print(f"Время выполнения решения №1: {result_time:.3f}")
 
 # Решение №2
+time_start = time.perf_counter()
+
+receptions_le_2017 = []
+for pat in patients:
+    for rec in receptions:
+        if rec["patient_id"] == pat["id"] and rec["reception_start"] <= datetime(2017, 1, 1):
+            receptions_le_2017.append(pat)
+            break
+
+result_time = time.perf_counter() - time_start
+print(f"Время выполнения решения №2: {result_time:.3f}")
+
+# Решение №1 O(len(patients) + len(receptions))
+# Решение №2 O(len(patients) * len(receptions))
+# №1 быстрее засчёт использования set
