a275b1aa79637735cf61249c6b23bf934bc27193
bonzermax <maksgames225@mail.ru>
2026-04-12 20:27:52 +0300

    update: Изменено решение №2 в задаче №5 с использованием pandas

--- task_5/task_5.py
+++ task_5/task_5.py
@@ -1,7 +1,8 @@
 import random
 import time
+import pandas as pd
+
 from datetime import datetime, timedelta
-from concurrent.futures import ProcessPoolExecutor
 
 random.seed(42)
 
@@ -19,10 +20,14 @@ patients = [{"id": pid, "surname": f"Иванов{pid}"} for pid in range(1, 500
 
 # Решение №1
 time_start = time.perf_counter()
+
 receptions_le_2017 = {
-    r["patient_id"] for r in receptions if r["reception_start"] <= datetime(2017, 1, 1)
+    r["patient_id"]
+    for r in receptions
+    if r["reception_start"] <= datetime(2017, 12, 31)
 }
 patients = [p for p in patients if p["id"] in receptions_le_2017]
+
 time_end = time.perf_counter()
 
 result_time = time_end - time_start
@@ -31,16 +36,26 @@ print(f"Время выполнения решения №1: {result_time:.3f}")
 # Решение №2
 time_start = time.perf_counter()
 
-receptions_le_2017 = []
-for pat in patients:
-    for rec in receptions:
-        if rec["patient_id"] == pat["id"] and rec["reception_start"] <= datetime(2017, 1, 1):
-            receptions_le_2017.append(pat)
-            break
+df_receptions = pd.DataFrame(receptions)
+df_patients = pd.DataFrame(patients)
+df = df_patients.merge(
+    df_receptions, left_on="id", right_on="patient_id", how="left"
+).drop(columns="patient_id")
 
-result_time = time.perf_counter() - time_start
+filtered_df = df.loc[(df["reception_start"] <= "2017-12-31")]
+filtered_patients = (
+    filtered_df[["id", "surname"]]
+    .drop_duplicates(subset="id")
+    .to_dict(orient="records")
+)
+
+time_end = time.perf_counter()
+
+result_time = time_end - time_start
 print(f"Время выполнения решения №2: {result_time:.3f}")
 
-# Решение №1 O(len(patients) + len(receptions))
-# Решение №2 O(len(patients) * len(receptions))
-# №1 быстрее засчёт использования set
+# Оба решения по сложности: O(len(patients) + len(receptions))
+# Время выполнения решения №1: 0.510
+# Время выполнения решения №2: 2.300
+
+# Вариант через pandas будет быстрее на больших объёмах данных, на небольших, как в нашем случае, выходит медленнее из-за процесса инициализации датафреймов
